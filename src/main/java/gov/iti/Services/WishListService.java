083211b45ff42d769e97d08d535cdc75234b99ea
Abdelrahman_Khaled Abouelsherbini <abdelrahman.elsheribini@gmail.com>
2025-04-14 16:43:15 +0200

    merge wish item

--- src/main/java/gov/iti/Services/WishListService.java
+++ src/main/java/gov/iti/Services/WishListService.java
@@ -3,6 +3,7 @@ package gov.iti.Services;
 
 import gov.iti.Entities.Wishlist;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 public class WishListService {
     private EntityManager entityManager;
@@ -17,6 +18,22 @@ public class WishListService {
     public boolean deleteWishList(Long wishlistId) {
         Wishlist wishList = crudService.find(wishlistId);
         return crudService.delete(wishList);
+    }
+
+    public boolean addWishList(Wishlist wishlist) {
+        TypedQuery<Wishlist> q = entityManager.createQuery("SELECT c FROM Wishlist c WHERE c.user = :user and c.product = :product", Wishlist.class);
+        q.setParameter("user", wishlist.getUser());
+        q.setParameter("product", wishlist.getProduct());
+        try {
+
+            Wishlist foundWish = q.getSingleResult();
+            return false;
+        } catch (Exception e) {
+
+        return crudService.create(wishlist);
+        }
+
+
     }
 
 }
